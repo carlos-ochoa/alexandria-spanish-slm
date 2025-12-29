8a5b42a8145846520a07bf330c37419145030609
Carlos Ochoa <caog.freeman@gmail.com>
2025-12-29 17:41:19 -0600

    feat: fix most common merges found

--- src/tools/data/dataset_processing.py
+++ src/tools/data/dataset_processing.py
@@ -1,6 +1,7 @@
 """This script contains the initial flow for preparing the dataset processed for training
 """
 
+from collections import Counter
 from datasets import load_dataset
 import unicodedata
 from tokenizer import AlexandriaTokenizer
@@ -11,7 +12,6 @@ def clean_data(row : str) -> str:
 
 ds = load_dataset("wikimedia/wikipedia", "20231101.es")
 ds = ds.map(clean_data)
-ds = ds["train"]["text"]
 
 tokenizer_ds = ds['train'].shuffle(seed=42).select(range(200000))
 tokenizer_ds = tokenizer_ds["text"]
@@ -31,12 +31,18 @@ for original_text, tokenized_text in zip(tokenizer_ds, tokenized_dataset):
     compression_ratio_byte_level += len(original_text.encode('utf-8')) / len(tokenized_text)
 compression_ratio_byte_level /= len(tokenized_dataset)
 
-print(compression_ratio_char_level)
-print(compression_ratio_byte_level)
+print(f"compression_ratio_char_level: {compression_ratio_char_level}")
+print(f"compression_ratio_byte_level: {compression_ratio_byte_level}")
 
-# Calculate the compression reason for the tokenizer
-# Visualize the top 20 most seen tokens for my tokenizer
+print("Most frequent merges in AlexandriaTokenizer")
+print(tokenizer.most_frequent_merges.most_common(20))
+for merge, _ in tokenizer.most_frequent_merges.most_common(20):
+   token_merge = tokenizer.merges[merge[0]]
+   print(token_merge , tokenizer.decode([token_merge]))
 
-
-
-
+print("Most frequent tokens discovered in unseen data")
+c = Counter()
+for t in tokenized_dataset:
+    c.update(t)
+for token in c.most_common(20):
+   print(tokenizer.decode(token))
