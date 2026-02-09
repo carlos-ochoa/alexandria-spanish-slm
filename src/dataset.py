07b91077e806b67f5c8595ecbd014a345ec0d89b
Carlos Ochoa <caog.freeman@gmail.com>
2026-02-08 20:51:58 -0600

    feat: build custom dataset for Alexandria

--- src/dataset.py
+++ src/dataset.py
@@ -1,16 +1,17 @@
-"""Defines the dataset for Alexandria
-"""
+"""Defines the dataset for Alexandria"""
 
-import torch
 from torch.utils.data import Dataset
 
-class WikipediaDataset(Dataset):
 
-    def __init__(self, tokenized_dataset_path : str):
-        self.tokenized_dataset = torch.load(tokenized_dataset_path)
+class AlexandriaDataset(Dataset):
+    def __init__(self, tokenized_dataset: list):
+        self.tokenized_dataset = tokenized_dataset["tokenized_docs"]
 
     def __len__(self):
         return len(self.tokenized_dataset)
 
     def __getitem__(self, index):
-        pass
+        # I have to return the input text and the target label
+        # Remember I can build many examples from the training data
+        # Is this place the best to perform that operation? Considering I'm returning single examples here
+        return self.tokenized_dataset[index][:-1], self.tokenized_dataset[index][1:]
