459aa64a7fb29551931d0d3c7a08873d620506b9
Carlos Ochoa <caog.freeman@gmail.com>
2026-02-08 20:47:52 -0600

    feat: add custom collate function

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import yaml
+
+from torch.nn.utils.rnn import pad_sequence
+
+
+class ConfigManager:
+    def __init__(self, path: str):
+        self.config = self._load_config(path)
+
+    def _load_config(self, path: str) -> dict:
+        with open(path, encoding="utf8") as f:
+            config = yaml.safe_load(f)
+        return config
+
+
+def create_collate_fn(pad_token_id=258):
+    def custom_padding_collate(batch):
+        tensors = [input_id for input_id, _ in batch]
+        labels = [label for _, label in batch]
+
+        padded_batch = pad_sequence(tensors, batch_first=True, padding_value=pad_token_id)
+
+        attention_mask = (padded_batch != pad_token_id).long()
+
+        padded_labels = pad_sequence(labels, batch_first=True, padding_value=pad_token_id)
+
+        return {
+            "input_ids": padded_batch,
+            "attention_mask": attention_mask,
+            "labels": padded_labels,
+        }
+
+    return custom_padding_collate
